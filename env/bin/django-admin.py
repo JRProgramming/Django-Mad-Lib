ac71a5aba90256c29a7da7ce191af588bd47dace
JRProgramming <ramirezjohnny@icloud.com>
2020-07-22 20:41:24 -0400

    Init commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/johnnyramirez/Documents/Coding Folder/Web Folder/Tutorials/Python Folder/Second Folder/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
